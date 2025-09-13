9209b35e5f591e7843d1466e89ab90db4e651503
wwwwyyt <wwwwyyt@126.com>
2025-09-13 21:54:31 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# 介绍
+
+这是一个基于 Vue3、Echarts 和 Vue Echarts 实现的数据可视化平台。
---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-chart class="chart" :option="option" autoresize />
+</template>
+
+<script setup>
+import { use } from 'echarts/core'
+import { LineChart } from 'echarts/charts'
+import { GridComponent } from 'echarts/components'
+import { CanvasRenderer } from 'echarts/renderers'
+
+use([GridComponent, LineChart, CanvasRenderer])
+
+import VChart, { THEME_KEY } from 'vue-echarts';
+import { ref, provide } from 'vue';
+
+provide(THEME_KEY, 'dark');
+
+const option = ref({
+  xAxis: {
+    type: 'category',
+    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+  },
+  yAxis: {
+    type: 'value'
+  },
+  series: [
+    {
+      data: [150, 230, 224, 218, 135, 147, 260],
+      type: 'line'
+    }
+  ]
+});
+
+</script>
+
+<style scoped>
+.chart {
+  height: 100vh;
+}
+</style>
